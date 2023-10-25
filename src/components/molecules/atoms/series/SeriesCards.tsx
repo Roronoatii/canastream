c420b2ef8d4698ec8592c87a07d2c62e93a7f458
Roronoatii <p.rivallin@protonmail.com>
2023-10-25 16:32:34 +0200

    subscription

--- src/components/molecules/atoms/series/SeriesCards.tsx
+++ src/components/molecules/atoms/series/SeriesCards.tsx
@@ -1,82 +1,109 @@
-import React from 'react';
-import { Box, IconButton, Stack, Typography } from '@mui/material';
-import AddIcon from '@mui/icons-material/Add';
-import CheckIcon from '@mui/icons-material/Check';
+import React from "react";
+import { Box, IconButton, Stack, Typography } from "@mui/material";
+import AddIcon from "@mui/icons-material/Add";
+import CheckIcon from "@mui/icons-material/Check";
+import { auth, firestore } from "../../../../database/firebase.config";
+import {
+  addDoc,
+  arrayUnion,
+  collection,
+  doc,
+  getDoc,
+  getDocs,
+  query,
+  updateDoc,
+  where,
+} from "firebase/firestore";
 
 interface SeriesCardProps {
-    id: number;
-    posterPath: string;
-    name: string;
-    genres: { id: number; name: string }[];
+  id: number;
+  posterPath: string;
+  name: string;
+  genres: { id: number; name: string }[];
 }
 
 export default function SeriesCard(props: SeriesCardProps) {
-    const { id, posterPath, name, genres } = props;
+  const { id, posterPath, name, genres } = props;
+  const handleSubscription = async (subscription: string) => {
+    const userRef = collection(firestore, "users");
+    if (auth.currentUser) {
+      const userQuery = query(userRef, where("id", "==", auth.currentUser.uid));
+      const querySnapshot = await getDocs(userQuery);
+      console.log(querySnapshot);
+      if (!querySnapshot.empty) {
+        const docSnapshot = querySnapshot.docs[0];
+        const userDocRef = doc(firestore, "users", docSnapshot.id);
 
-    return (
-        <Box key={id} sx={{ width: '100%', position: 'relative' }}>
-            <img
-                src={`https://image.tmdb.org/t/p/w185${posterPath}`}
-                alt={name}
-                style={{ maxWidth: '100%', height: 'auto' }}
-            />
-            if (isInWatchlist) {
-           <IconButton
-                sx={{
-                    position: 'absolute',
-                    top: '5%',
-                    right: '35%',
-                    backgroundColor: '#499b4a',
-                    borderRadius: '5px',
-                    color: '#000000',
-                    '&:hover': {
-                        backgroundColor: '#499b4a',
-                    },
-                }}>
-                    <CheckIcon />
-                </IconButton>
-            } else {
-                <IconButton
-                    onClick={
-                        () => {
-                            console.log('Add to watchlist');
-                        }
-                    }
-                    sx={{
-                        position: 'absolute',
-                        top: '5%',
-                        right: '35%',
-                        backgroundColor: '#e0e0e0',
-                        borderRadius: '5px',
-                        color: '#000000',
-                        '&:hover': {
-                            backgroundColor: '#499b4a',
-                        },
-                    }}
-                >
-                <AddIcon />
-            </IconButton>
-            }
+        await updateDoc(userDocRef, {
+          subscriptions: arrayUnion(subscription),
+        });
+      }
+    }
+  };
 
-            <Typography sx={{ fontSize: '12px', transform: 'uppercase', mt: '5px' }}>
-                {name}
-            </Typography>
-            <Stack
-                sx={{
-                    borderRadius: '5px',
-                    backgroundColor: '#e0e0e0',
-                    color: '#000000',
-                    fontSize: '12px',
-                    textAlign: 'center',
-                    width: 'fit-content',
-                    p: '5px',
-                    cursor: 'pointer'
-                }}
-            >
-                {genres.length > 0
-                    ? genres.find(g => g.id === genres[0].id)?.name || 'Unknown Genre'
-                    : 'Unknown Genre'}
-            </Stack>
-        </Box>
-    );
+  return (
+    <Box key={id} sx={{ width: "100%", position: "relative" }}>
+      <img
+        src={`https://image.tmdb.org/t/p/w185${posterPath}`}
+        alt={name}
+        style={{ maxWidth: "100%", height: "auto" }}
+      />
+      if (isInWatchlist){" "}
+      {
+        <IconButton
+          sx={{
+            position: "absolute",
+            top: "5%",
+            right: "35%",
+            backgroundColor: "#499b4a",
+            borderRadius: "5px",
+            color: "#000000",
+            "&:hover": {
+              backgroundColor: "#499b4a",
+            },
+          }}
+        >
+          <CheckIcon />
+        </IconButton>
+      }{" "}
+      else{" "}
+      {
+        <IconButton
+          onClick={() => handleSubscription(name)}
+          sx={{
+            position: "absolute",
+            top: "5%",
+            right: "35%",
+            backgroundColor: "#e0e0e0",
+            borderRadius: "5px",
+            color: "#000000",
+            "&:hover": {
+              backgroundColor: "#499b4a",
+            },
+          }}
+        >
+          <AddIcon />
+        </IconButton>
+      }
+      <Typography sx={{ fontSize: "12px", transform: "uppercase", mt: "5px" }}>
+        {name}
+      </Typography>
+      <Stack
+        sx={{
+          borderRadius: "5px",
+          backgroundColor: "#e0e0e0",
+          color: "#000000",
+          fontSize: "12px",
+          textAlign: "center",
+          width: "fit-content",
+          p: "5px",
+          cursor: "pointer",
+        }}
+      >
+        {genres.length > 0
+          ? genres.find((g) => g.id === genres[0].id)?.name || "Unknown Genre"
+          : "Unknown Genre"}
+      </Stack>
+    </Box>
+  );
 }
--- src/views/Home/Home.tsx
+++ src/views/Home/Home.tsx
@@ -1,122 +1,124 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from "react";
 import {
   Stack,
   IconButton,
   InputBase,
   Paper,
   Box,
-  Typography
-} from '@mui/material'
-import SearchIcon from '@mui/icons-material/Search'
-import axios from 'axios'
-import AddIcon from '@mui/icons-material/Add'
-import ArrowBackIcon from '@mui/icons-material/ArrowBack'
-import ArrowForwardIcon from '@mui/icons-material/ArrowForward'
-import SeriesCard from '../../components/molecules/atoms/series/SeriesCards'
+  Typography,
+} from "@mui/material";
+import SearchIcon from "@mui/icons-material/Search";
+import axios from "axios";
+import AddIcon from "@mui/icons-material/Add";
+import ArrowBackIcon from "@mui/icons-material/ArrowBack";
+import ArrowForwardIcon from "@mui/icons-material/ArrowForward";
+import SeriesCard from "../../components/molecules/atoms/series/SeriesCards";
 
 interface Genre {
-  id: number
-  name: string
+  id: number;
+  name: string;
 }
 
 interface TVShow {
-  id: number
-  name: string
-  genre_ids: number[]
-  poster_path: string
+  id: number;
+  name: string;
+  genre_ids: number[];
+  poster_path: string;
 }
 
 const Home = () => {
-  const [genres, setGenres] = useState<Genre[]>([])
-  const [activeGenre, setActiveGenre] = useState<string>('All')
-  const [tvShows, setTVShows] = useState<TVShow[]>([])
-  const [searchQuery, setSearchQuery] = useState<string>('')
-  const apiKey = '2955ed558f1e71d9871ec2a96694678a'
-  const [currentPage, setCurrentPage] = useState(1)
-  const showsPerRow = 5
-  const rowsPerPage = 4
+  const [genres, setGenres] = useState<Genre[]>([]);
+  const [activeGenre, setActiveGenre] = useState<string>("All");
+  const [tvShows, setTVShows] = useState<TVShow[]>([]);
+  const [searchQuery, setSearchQuery] = useState<string>("");
+  const apiKey = "2955ed558f1e71d9871ec2a96694678a";
+  const [currentPage, setCurrentPage] = useState(1);
+  const showsPerRow = 5;
+  const rowsPerPage = 4;
 
   useEffect(() => {
     axios
       .get(`https://api.themoviedb.org/3/genre/tv/list?api_key=${apiKey}`)
       .then((response: { data: { genres: Genre[] } }) => {
-        setGenres(response.data.genres)
+        setGenres(response.data.genres);
       })
       .catch((error: any) => {
-        console.error(error)
-      })
+        console.error(error);
+      });
 
     axios
       .get(`https://api.themoviedb.org/3/tv/popular?api_key=${apiKey}`)
       .then((response: { data: { results: TVShow[] } }) => {
-        setTVShows(response.data.results)
+        setTVShows(response.data.results);
       })
       .catch((error: any) => {
-        console.error(error)
-      })
-  }, [])
+        console.error(error);
+      });
+  }, []);
 
   const handleGenreClick = (genreName: string) => {
-    setActiveGenre(genreName)
-  }
+    setActiveGenre(genreName);
+  };
 
   const handleSearchChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setSearchQuery(event.target.value)
-  }
+    setSearchQuery(event.target.value);
+  };
 
-  const filteredTVShows = tvShows.filter(show => {
+  const filteredTVShows = tvShows.filter((show) => {
     const genreMatch =
-      activeGenre === 'All' ||
-      show.genre_ids.includes(genres.find(g => g.name === activeGenre)?.id || 0)
+      activeGenre === "All" ||
+      show.genre_ids.includes(
+        genres.find((g) => g.name === activeGenre)?.id || 0
+      );
     const searchMatch = show.name
       .toLowerCase()
-      .includes(searchQuery.toLowerCase())
-    return genreMatch && searchMatch
-  })
+      .includes(searchQuery.toLowerCase());
+    return genreMatch && searchMatch;
+  });
 
-  const startIndex = (currentPage - 1) * rowsPerPage * showsPerRow
-  const endIndex = startIndex + rowsPerPage * showsPerRow
+  const startIndex = (currentPage - 1) * rowsPerPage * showsPerRow;
+  const endIndex = startIndex + rowsPerPage * showsPerRow;
 
-  const paginatedTVShows = filteredTVShows.slice(startIndex, endIndex)
+  const paginatedTVShows = filteredTVShows.slice(startIndex, endIndex);
 
   const nextPage = () => {
-    setCurrentPage(currentPage + 1)
-  }
+    setCurrentPage(currentPage + 1);
+  };
 
   const prevPage = () => {
     if (currentPage > 1) {
-      setCurrentPage(currentPage - 1)
+      setCurrentPage(currentPage - 1);
     }
-  }
+  };
 
   return (
     <Stack>
       <Stack>
-        <Paper component='form' sx={{ borderRadius: '5px', mb: '10px' }}>
-          <IconButton aria-label='search'>
+        <Paper component="form" sx={{ borderRadius: "5px", mb: "10px" }}>
+          <IconButton aria-label="search">
             <SearchIcon />
           </IconButton>
           <InputBase
-            placeholder='Search…'
-            inputProps={{ 'aria-label': 'search' }}
+            placeholder="Search…"
+            inputProps={{ "aria-label": "search" }}
             value={searchQuery}
             onChange={handleSearchChange}
-            sx={{ ml: 1, flex: 1, width: '95%' }}
+            sx={{ ml: 1, flex: 1, width: "95%" }}
           />
         </Paper>
-        <Stack direction='row' sx={{ mb: '10px' }}>
+        <Stack direction="row" sx={{ mb: "10px" }}>
           <Stack
             sx={{
-              borderRadius: '5px',
-              backgroundColor: activeGenre === 'All' ? '#499b4a' : '#e0e0e0',
-              color: '#000000',
-              textAlign: 'center',
-              fontSize: '12px',
-              p: '5px',
-              mx: '5px',
-              cursor: 'pointer'
+              borderRadius: "5px",
+              backgroundColor: activeGenre === "All" ? "#499b4a" : "#e0e0e0",
+              color: "#000000",
+              textAlign: "center",
+              fontSize: "12px",
+              p: "5px",
+              mx: "5px",
+              cursor: "pointer",
             }}
-            onClick={() => handleGenreClick('All')}
+            onClick={() => handleGenreClick("All")}
           >
             All
           </Stack>
@@ -124,15 +126,15 @@ const Home = () => {
             <Stack
               key={genre.id}
               sx={{
-                borderRadius: '5px',
+                borderRadius: "5px",
                 backgroundColor:
-                  activeGenre === genre.name ? '#499b4a' : '#e0e0e0',
-                color: '#000000',
-                fontSize: '12px',
-                textAlign: 'center',
-                p: '5px',
-                mx: '5px',
-                cursor: 'pointer'
+                  activeGenre === genre.name ? "#499b4a" : "#e0e0e0",
+                color: "#000000",
+                fontSize: "12px",
+                textAlign: "center",
+                p: "5px",
+                mx: "5px",
+                cursor: "pointer",
               }}
               onClick={() => handleGenreClick(genre.name)}
             >
@@ -146,10 +148,10 @@ const Home = () => {
         <Box
           key={rowIndex}
           sx={{
-            display: 'grid',
+            display: "grid",
             gridTemplateColumns: `repeat(${showsPerRow}, 1fr)`,
-            gap: '10px',
-            mb: '10px'
+            gap: "10px",
+            mb: "10px",
           }}
         >
           {paginatedTVShows
@@ -160,10 +162,11 @@ const Home = () => {
                 id={show.id}
                 posterPath={show.poster_path}
                 name={show.name}
-                genres={show.genre_ids.map(genre_id => ({
+                genres={show.genre_ids.map((genre_id) => ({
                   id: genre_id,
                   name:
-                    genres.find(g => g.id === genre_id)?.name || 'Unknown Genre'
+                    genres.find((g) => g.id === genre_id)?.name ||
+                    "Unknown Genre",
                 }))}
               />
             ))}
@@ -171,8 +174,8 @@ const Home = () => {
       ))}
 
       <Stack
-        direction='row'
-        sx={{ justifyContent: 'center', alignItems: 'center', mt: '10px' }}
+        direction="row"
+        sx={{ justifyContent: "center", alignItems: "center", mt: "10px" }}
       >
         <IconButton disabled={currentPage === 1}>
           <ArrowBackIcon onClick={prevPage} />
@@ -182,7 +185,7 @@ const Home = () => {
           totalPages={Math.ceil(
             filteredTVShows.length / (rowsPerPage * showsPerRow)
           )}
-          onPageClick={page => setCurrentPage(page)}
+          onPageClick={(page) => setCurrentPage(page)}
         />
         <IconButton
           disabled={
@@ -193,40 +196,40 @@ const Home = () => {
         </IconButton>
       </Stack>
     </Stack>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
 
 type PaginationProps = {
-  currentPage: number
-  totalPages: number
-  onPageClick: (page: number) => void
-}
+  currentPage: number;
+  totalPages: number;
+  onPageClick: (page: number) => void;
+};
 
 const Pagination = ({
   currentPage,
   totalPages,
-  onPageClick
+  onPageClick,
 }: PaginationProps) => {
-  const pageNumbers = Array.from({ length: totalPages }, (_, i) => i + 1)
+  const pageNumbers = Array.from({ length: totalPages }, (_, i) => i + 1);
 
   return (
-    <Stack sx={{ flexDirection: 'row', alignItems: 'center' }}>
-      {pageNumbers.map(page => (
+    <Stack sx={{ flexDirection: "row", alignItems: "center" }}>
+      {pageNumbers.map((page) => (
         <Typography
           key={page}
           onClick={() => onPageClick(page)}
           sx={{
-            cursor: 'pointer',
-            color: currentPage === page ? '#499b4a' : '#000000',
-            fontSize: '16px',
-            p: '5px'
+            cursor: "pointer",
+            color: currentPage === page ? "#499b4a" : "#000000",
+            fontSize: "16px",
+            p: "5px",
           }}
         >
           {page}
         </Typography>
       ))}
     </Stack>
-  )
-}
+  );
+};
