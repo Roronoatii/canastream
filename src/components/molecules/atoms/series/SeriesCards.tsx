26928f827340380d41161a278bfb9271de7d6180
Roronoatii <p.rivallin@protonmail.com>
2023-10-27 12:14:22 +0200

    calendar back

--- src/components/molecules/atoms/series/SeriesCards.tsx
+++ src/components/molecules/atoms/series/SeriesCards.tsx
@@ -1,15 +1,15 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect } from "react";
 import {
   Box,
   IconButton,
   Stack,
   Typography,
   ImageListItem,
-  ImageListItemBar
-} from '@mui/material'
-import AddIcon from '@mui/icons-material/Add'
-import CheckIcon from '@mui/icons-material/Check'
-import { auth, firestore } from '../../../../database/firebase.config'
+  ImageListItemBar,
+} from "@mui/material";
+import AddIcon from "@mui/icons-material/Add";
+import CheckIcon from "@mui/icons-material/Check";
+import { auth, firestore } from "../../../../database/firebase.config";
 import {
   arrayUnion,
   collection,
@@ -18,136 +18,139 @@ import {
   query,
   updateDoc,
   where,
-  onSnapshot
-} from 'firebase/firestore'
-import { Link } from 'react-router-dom'
+  onSnapshot,
+} from "firebase/firestore";
+import { Link } from "react-router-dom";
 
 interface SeriesCardProps {
-  id: number
-  posterPath: string
-  name: string
-  genres: { id: number; name: string }[]
+  id: number;
+  posterPath: string;
+  name: string;
+  genres: { id: number; name: string }[];
 }
 
 export default function SeriesCard(props: SeriesCardProps) {
-  const { id, posterPath, name, genres } = props
-  const [isSubscribed, setIsSubscribed] = useState(false)
+  const { id, posterPath, name, genres } = props;
+  const [isSubscribed, setIsSubscribed] = useState(false);
 
-  const handleSubscription = async (subscription: string) => {
-    const userRef = collection(firestore, 'users')
+  const handleSubscription = async (subscription: number) => {
+    const userRef = collection(firestore, "users");
     if (auth.currentUser) {
-      const userQuery = query(userRef, where('id', '==', auth.currentUser.uid))
-      const querySnapshot = await getDocs(userQuery)
-      console.log(querySnapshot)
+      const userQuery = query(userRef, where("id", "==", auth.currentUser.uid));
+      const querySnapshot = await getDocs(userQuery);
+      console.log(querySnapshot);
       if (!querySnapshot.empty) {
-        const docSnapshot = querySnapshot.docs[0]
-        const userDocRef = doc(firestore, 'users', docSnapshot.id)
+        const docSnapshot = querySnapshot.docs[0];
+        const userDocRef = doc(firestore, "users", docSnapshot.id);
 
         await updateDoc(userDocRef, {
-          subscriptions: arrayUnion(subscription)
-        })
+          subscriptions: arrayUnion(subscription),
+        });
       }
     }
-  }
+  };
 
   useEffect(() => {
     async function checkSubscription() {
       if (auth.currentUser) {
-        const userRef = collection(firestore, 'users')
+        const userRef = collection(firestore, "users");
         const userQuery = query(
           userRef,
-          where('id', '==', auth.currentUser.uid)
-        )
-        const querySnapshot = await getDocs(userQuery)
+          where("id", "==", auth.currentUser.uid)
+        );
+        const querySnapshot = await getDocs(userQuery);
 
         if (!querySnapshot.empty) {
-          const docSnapshot = querySnapshot.docs[0]
-          const userData = docSnapshot.data()
-          if (userData.subscriptions.includes(name)) {
-            setIsSubscribed(true)
+          const docSnapshot = querySnapshot.docs[0];
+          const userData = docSnapshot.data();
+          if (userData.subscriptions.includes(id)) {
+            setIsSubscribed(true);
           } else {
-            setIsSubscribed(false)
+            setIsSubscribed(false);
           }
         }
       }
     }
 
-    checkSubscription()
+    checkSubscription();
 
-    const unsubscribe = onSnapshot(collection(firestore, 'users'), snapshot => {
-      snapshot.docs.forEach(doc => {
-        const userData = doc.data()
-        if (userData.id === auth.currentUser?.uid) {
-          if (userData.subscriptions.includes(name)) {
-            setIsSubscribed(true)
-          } else {
-            setIsSubscribed(false)
+    const unsubscribe = onSnapshot(
+      collection(firestore, "users"),
+      (snapshot) => {
+        snapshot.docs.forEach((doc) => {
+          const userData = doc.data();
+          if (userData.id === auth.currentUser?.uid) {
+            if (userData.subscriptions.includes(id)) {
+              setIsSubscribed(true);
+            } else {
+              setIsSubscribed(false);
+            }
           }
-        }
-      })
-    })
+        });
+      }
+    );
 
-    return () => unsubscribe()
-  }, [name])
+    return () => unsubscribe();
+  }, [id]);
 
   return (
-    <Box key={id} sx={{ width: '75%', position: 'relative', mx: 'auto' }}>
+    <Box key={id} sx={{ width: "75%", position: "relative", mx: "auto" }}>
       <Link
         to={`/series/${id}`}
-        style={{ textDecoration: 'none', color: 'inherit' }}
+        style={{ textDecoration: "none", color: "inherit" }}
       >
         <ImageListItem>
           <img
             src={`https://image.tmdb.org/t/p/w185${posterPath}`}
             alt={name}
-            style={{ maxWidth: '100%', height: 'auto' }}
+            style={{ maxWidth: "100%", height: "auto" }}
           />
           <ImageListItemBar
-            sx={{ mx: '5%', my: '5%', background: 'transparent' }}
+            sx={{ mx: "5%", my: "5%", background: "transparent" }}
             actionIcon={
               <IconButton
                 sx={{
-                  backgroundColor: isSubscribed ? '#499b4a' : '#e0e0e0',
-                  borderRadius: '5px',
-                  color: '#000000',
-                  '&:hover': {
-                    backgroundColor: '#499b4a'
-                  }
+                  backgroundColor: isSubscribed ? "#499b4a" : "#e0e0e0",
+                  borderRadius: "5px",
+                  color: "#000000",
+                  "&:hover": {
+                    backgroundColor: "#499b4a",
+                  },
                 }}
-                onClick={e => {
-                  e.preventDefault()
-                  handleSubscription(name)
+                onClick={(e) => {
+                  e.preventDefault();
+                  handleSubscription(id);
                 }}
               >
-                {isSubscribed ? <CheckIcon /> : <AddIcon />}{' '}
+                {isSubscribed ? <CheckIcon /> : <AddIcon />}{" "}
               </IconButton>
             }
           />
         </ImageListItem>
         <Typography
-          variant='button'
-          sx={{ fontSize: '12px', mt: '5px', textDecoration: 'none' }}
+          variant="button"
+          sx={{ fontSize: "12px", mt: "5px", textDecoration: "none" }}
         >
           {name}
         </Typography>
         <Stack
           sx={{
-            borderRadius: '5px',
-            backgroundColor: '#e0e0e0',
-            color: '#000000',
-            textDecoration: 'none',
-            fontSize: '12px',
-            textAlign: 'center',
-            width: 'fit-content',
-            p: '5px',
-            cursor: 'pointer'
+            borderRadius: "5px",
+            backgroundColor: "#e0e0e0",
+            color: "#000000",
+            textDecoration: "none",
+            fontSize: "12px",
+            textAlign: "center",
+            width: "fit-content",
+            p: "5px",
+            cursor: "pointer",
           }}
         >
           {genres.length > 0
-            ? genres.find(g => g.id === genres[0].id)?.name || 'Unknown Genre'
-            : 'Unknown Genre'}
+            ? genres.find((g) => g.id === genres[0].id)?.name || "Unknown Genre"
+            : "Unknown Genre"}
         </Stack>
       </Link>
     </Box>
-  )
+  );
 }
--- src/views/Details/SerieDetails.tsx
+++ src/views/Details/SerieDetails.tsx
@@ -1,6 +1,6 @@
-import React, { useEffect, useState } from 'react'
-import { useParams, useNavigate } from 'react-router-dom'
-import axios from 'axios'
+import React, { useEffect, useState } from "react";
+import { useParams, useNavigate } from "react-router-dom";
+import axios from "axios";
 import {
   Container,
   Typography,
@@ -9,8 +9,8 @@ import {
   TextareaAutosize,
   Button,
   Box,
-  Rating
-} from '@mui/material'
+  Rating,
+} from "@mui/material";
 import {
   collection,
   query,
@@ -20,316 +20,324 @@ import {
   updateDoc,
   arrayUnion,
   onSnapshot,
-  addDoc
-} from 'firebase/firestore'
-import { User, onAuthStateChanged } from 'firebase/auth'
-import { firestore, auth } from '../../database/firebase.config'
-import SerieInfo from '../../components/Details/SerieInfo'
-import SubscriptionButton from '../../components/Details/SubscriptionButton'
-import SeasonList from '../../components/Details/SeasonList'
-import CastList from '../../components/Details/CastList'
-import ReviewList from '../../components/Details/ReviewList'
+  addDoc,
+} from "firebase/firestore";
+import { User, onAuthStateChanged } from "firebase/auth";
+import { firestore, auth } from "../../database/firebase.config";
+import SerieInfo from "../../components/Details/SerieInfo";
+import SubscriptionButton from "../../components/Details/SubscriptionButton";
+import SeasonList from "../../components/Details/SeasonList";
+import CastList from "../../components/Details/CastList";
+import ReviewList from "../../components/Details/ReviewList";
 
-import { SeriesDetails } from '../../components/Details/SerieInfo'
-import { Season } from '../../components/Details/SeasonList'
-import { Review } from '../../components/Details/ReviewList'
-import { CastMember } from '../../components/Details/CastList'
-import SpaIcon from '@mui/icons-material/Spa'
-
+import { SeriesDetails } from "../../components/Details/SerieInfo";
+import { Season } from "../../components/Details/SeasonList";
+import { Review } from "../../components/Details/ReviewList";
+import { CastMember } from "../../components/Details/CastList";
+import SpaIcon from "@mui/icons-material/Spa";
 
 const SerieDetails = () => {
-  const navigate = useNavigate()
-  const { seriesId } = useParams()
-  const [seriesDetails, setSeriesDetails] = useState<SeriesDetails | null>(null)
-  const [seasons, setSeasons] = useState<Season[]>([])
-  const [cast, setCast] = useState<CastMember[]>([])
-  const [isSubscribed, setIsSubscribed] = useState(false)
-  const [rating, setRating] = useState<number>(0)
-  const [comment, setComment] = useState('')
-  const [reviews, setReviews] = useState<Review[]>([])
-  const [user, setUser] = useState<User | null>(null)
+  const navigate = useNavigate();
+  const { seriesId } = useParams();
+  const [seriesDetails, setSeriesDetails] = useState<SeriesDetails | null>(
+    null
+  );
 
-  const apiKey = '2955ed558f1e71d9871ec2a96694678a'
+  const [seasons, setSeasons] = useState<Season[]>([]);
+  const [cast, setCast] = useState<CastMember[]>([]);
+  const [isSubscribed, setIsSubscribed] = useState(false);
+  const [rating, setRating] = useState<number>(0);
+  const [comment, setComment] = useState("");
+  const [reviews, setReviews] = useState<Review[]>([]);
+  const [user, setUser] = useState<User | null>(null);
+
+  const apiKey = "2955ed558f1e71d9871ec2a96694678a";
 
   const handleSubscription = async (subscription: string) => {
-    const userRef = collection(firestore, 'users')
+    const userRef = collection(firestore, "users");
     if (auth.currentUser) {
-      const userQuery = query(userRef, where('id', '==', auth.currentUser.uid))
-      const querySnapshot = await getDocs(userQuery)
-      console.log(querySnapshot)
+      const userQuery = query(userRef, where("id", "==", auth.currentUser.uid));
+      const querySnapshot = await getDocs(userQuery);
+      console.log(querySnapshot);
       if (!querySnapshot.empty) {
-        const docSnapshot = querySnapshot.docs[0]
-        const userDocRef = doc(firestore, 'users', docSnapshot.id)
+        const docSnapshot = querySnapshot.docs[0];
+        const userDocRef = doc(firestore, "users", docSnapshot.id);
 
         await updateDoc(userDocRef, {
-          subscriptions: arrayUnion(subscription)
-        })
+          subscriptions: arrayUnion(subscription),
+        });
       }
     }
-  }
+  };
 
   const handleUnsubscription = async (subscription: string) => {
     if (auth.currentUser && seriesDetails) {
-      const userRef = collection(firestore, 'users')
-      const userQuery = query(userRef, where('id', '==', auth.currentUser.uid))
-      const querySnapshot = await getDocs(userQuery)
+      const userRef = collection(firestore, "users");
+      const userQuery = query(userRef, where("id", "==", auth.currentUser.uid));
+      const querySnapshot = await getDocs(userQuery);
 
       if (!querySnapshot.empty) {
-        const docSnapshot = querySnapshot.docs[0]
-        const userDocRef = doc(firestore, 'users', docSnapshot.id)
+        const docSnapshot = querySnapshot.docs[0];
+        const userDocRef = doc(firestore, "users", docSnapshot.id);
 
-        const userData = docSnapshot.data()
+        const userData = docSnapshot.data();
         if (userData.subscriptions.includes(subscription)) {
           await updateDoc(userDocRef, {
             subscriptions: userData.subscriptions.filter(
               (item: string) => item !== subscription
-            )
-          })
+            ),
+          });
         }
       }
     }
-  }
+  };
 
   const toggleSubscription = () => {
     if (seriesDetails) {
       if (isSubscribed) {
-        handleUnsubscription(seriesDetails.name)
+        handleUnsubscription(seriesDetails.id);
       } else {
-        handleSubscription(seriesDetails.name)
+        handleSubscription(seriesDetails.id);
       }
     }
-  }
+  };
 
   useEffect(() => {
     axios
       .get(`https://api.themoviedb.org/3/tv/${seriesId}?api_key=${apiKey}`)
-      .then(response => {
-        setSeriesDetails(response.data)
+      .then((response) => {
+        setSeriesDetails(response.data);
       })
-      .catch(error => {
-        console.error(error)
-      })
+      .catch((error) => {
+        console.error(error);
+      });
 
     axios
       .get(
         `https://api.themoviedb.org/3/tv/${seriesId}?api_key=${apiKey}&append_to_response=seasons`
       )
-      .then(response => {
-        setSeasons(response.data.seasons)
+      .then((response) => {
+        setSeasons(response.data.seasons);
       })
-      .catch(error => {
-        console.error(error)
-      })
+      .catch((error) => {
+        console.error(error);
+      });
 
     axios
       .get(
         `https://api.themoviedb.org/3/tv/${seriesId}/credits?api_key=${apiKey}`
       )
-      .then(response => {
-        setCast(response.data.cast)
+      .then((response) => {
+        setCast(response.data.cast);
       })
-      .catch(error => {
-        console.error(error)
-      })
-  }, [seriesId])
+      .catch((error) => {
+        console.error(error);
+      });
+  }, [seriesId]);
 
   useEffect(() => {
     const unsubscribe = onAuthStateChanged(auth, (currentUser: User | null) => {
       if (currentUser) {
-        setUser(currentUser)
+        setUser(currentUser);
       }
-    })
+    });
 
-    return () => unsubscribe()
-  }, [navigate, auth])
+    return () => unsubscribe();
+  }, [navigate, auth]);
 
   useEffect(() => {
     async function checkSubscription() {
       if (auth.currentUser && seriesDetails) {
-        const userRef = collection(firestore, 'users')
+        const userRef = collection(firestore, "users");
         const userQuery = query(
           userRef,
-          where('id', '==', auth.currentUser.uid)
-        )
-        const querySnapshot = await getDocs(userQuery)
+          where("id", "==", auth.currentUser.uid)
+        );
+        const querySnapshot = await getDocs(userQuery);
 
         if (!querySnapshot.empty) {
-          const docSnapshot = querySnapshot.docs[0]
-          const userData = docSnapshot.data()
-          if (userData.subscriptions.includes(seriesDetails.name)) {
-            setIsSubscribed(true)
+          const docSnapshot = querySnapshot.docs[0];
+          const userData = docSnapshot.data();
+          if (userData.subscriptions.includes(seriesDetails.id)) {
+            setIsSubscribed(true);
           } else {
-            setIsSubscribed(false)
+            setIsSubscribed(false);
           }
         }
       }
     }
 
-    checkSubscription()
+    checkSubscription();
 
-    const unsubscribe = onSnapshot(collection(firestore, 'users'), snapshot => {
-      snapshot.docs.forEach(doc => {
-        const userData = doc.data()
-        if (userData.id === auth.currentUser?.uid && seriesDetails) {
-          if (userData.subscriptions.includes(seriesDetails.name)) {
-            setIsSubscribed(true)
-          } else {
-            setIsSubscribed(false)
+    const unsubscribe = onSnapshot(
+      collection(firestore, "users"),
+      (snapshot) => {
+        snapshot.docs.forEach((doc) => {
+          const userData = doc.data();
+          if (userData.id === auth.currentUser?.uid && seriesDetails) {
+            if (userData.subscriptions.includes(seriesDetails.id)) {
+              setIsSubscribed(true);
+            } else {
+              setIsSubscribed(false);
+            }
           }
-        }
-      })
-    })
+        });
+      }
+    );
 
-    return () => unsubscribe()
-  }, [seriesDetails, auth.currentUser])
+    return () => unsubscribe();
+  }, [seriesDetails, auth.currentUser]);
 
   const submitReview = async () => {
     if (auth.currentUser) {
-      const now = new Date()
-      const hours = now.getHours()
-      const minutes = now.getMinutes()
+      const now = new Date();
+      const hours = now.getHours();
+      const minutes = now.getMinutes();
       const reviewData = {
         userId: auth.currentUser.uid,
         userName: auth.currentUser.displayName,
         seriesId: seriesId,
         rating: rating,
         comment: comment,
-        timestamp: Date.now()
-      }
+        timestamp: Date.now(),
+      };
 
-      const docRef = await addDoc(collection(firestore, 'reviews'), reviewData)
+      const docRef = await addDoc(collection(firestore, "reviews"), reviewData);
 
-      setRating(0)
-      setComment('')
+      setRating(0);
+      setComment("");
 
-      const newReviewId = docRef.id
-      const newReview = { ...reviewData, id: newReviewId }
+      const newReviewId = docRef.id;
+      const newReview = { ...reviewData, id: newReviewId };
 
-      setReviews(prevReviews => [...prevReviews, newReview])
+      setReviews((prevReviews) => [...prevReviews, newReview]);
 
-      console.log('Avis ajouté avec ID : ', docRef.id)
+      console.log("Avis ajouté avec ID : ", docRef.id);
     } else {
-      navigate('/login')
+      navigate("/login");
     }
-  }
+  };
 
   useEffect(() => {
     async function fetchReviews() {
-      const reviewsRef = collection(firestore, 'reviews')
-      const reviewsQuery = query(reviewsRef, where('seriesId', '==', seriesId))
-      const querySnapshot = await getDocs(reviewsQuery)
+      const reviewsRef = collection(firestore, "reviews");
+      const reviewsQuery = query(reviewsRef, where("seriesId", "==", seriesId));
+      const querySnapshot = await getDocs(reviewsQuery);
 
-      const reviewsData: Review[] = []
-      querySnapshot.forEach(doc => {
-        const review = doc.data() as Review
-        reviewsData.push(review)
-      })
+      const reviewsData: Review[] = [];
+      querySnapshot.forEach((doc) => {
+        const review = doc.data() as Review;
+        reviewsData.push(review);
+      });
 
-      setReviews(reviewsData)
+      setReviews(reviewsData);
     }
 
-    fetchReviews()
-  }, [seriesId])
+    fetchReviews();
+  }, [seriesId]);
 
   if (!seriesDetails) {
-    return <div>Loading...</div>
+    return <div>Loading...</div>;
   }
 
   if (user) {
-    const currentDisplayName = user.displayName || ''
+    const currentDisplayName = user.displayName || "";
   }
 
   const styles = {
     paperContainer: {
       backgroundImage: `url(https://image.tmdb.org/t/p/w500${seriesDetails.backdrop_path})`,
-      backgroundSize: 'cover',
-      height: '75vh',
-      padding: '16px',
-      color: 'white',
-      position: 'relative'
+      backgroundSize: "cover",
+      height: "75vh",
+      padding: "16px",
+      color: "white",
+      position: "relative",
     },
     darkOverlay: {
       content: '""',
-      position: 'absolute',
-      display: 'flex',
-      flexDirection: 'column',
-      justifyContent: 'flex-end',
-      padding: '16px',
+      position: "absolute",
+      display: "flex",
+      flexDirection: "column",
+      justifyContent: "flex-end",
+      padding: "16px",
       top: 0,
       left: 0,
-      width: '100%',
-      height: '100%',
-      backgroundColor: 'rgba(0, 0, 0, 0.5)'
+      width: "100%",
+      height: "100%",
+      backgroundColor: "rgba(0, 0, 0, 0.5)",
     },
     buttonContainer: {
-      position: 'absolute',
-      top: '16px',
-      left: '16px',
-      zIndex: 1
+      position: "absolute",
+      top: "16px",
+      left: "16px",
+      zIndex: 1,
     },
-  }
+  };
 
   return (
-    <Container sx={{mb: 2}}>
+    <Container sx={{ mb: 2 }}>
       <Paper sx={styles.paperContainer}>
         <Box sx={styles.darkOverlay}>
           <Stack sx={styles.buttonContainer}>
-          <SubscriptionButton isSubscribed={isSubscribed} onClick={toggleSubscription} />
+            <SubscriptionButton
+              isSubscribed={isSubscribed}
+              onClick={toggleSubscription}
+            />
           </Stack>
           <SerieInfo seriesDetails={seriesDetails} />
         </Box>
       </Paper>
-      <Typography variant='h5' sx={{ mt: 2, mb: 2 }}>
+      <Typography variant="h5" sx={{ mt: 2, mb: 2 }}>
         Seasons
       </Typography>
       <SeasonList seasons={seasons} />
-      <Typography variant='h5' sx={{ mt: 4 }}>
+      <Typography variant="h5" sx={{ mt: 4 }}>
         Cast
       </Typography>
       <CastList cast={cast} />
 
-      <Stack direction='column'>
-        <Typography variant='h5' sx={{ my: 4 }}>
+      <Stack direction="column">
+        <Typography variant="h5" sx={{ my: 4 }}>
           Add a Review
         </Typography>
-        <Stack direction='column' sx={{ mb: 2, alignItems: 'center' }}>
+        <Stack direction="column" sx={{ mb: 2, alignItems: "center" }}>
           <Rating
-            name='simple-controlled'
+            name="simple-controlled"
             value={rating}
-            onChange={e =>
+            onChange={(e) =>
               setRating(parseInt((e.target as HTMLSelectElement).value))
             }
             size="large"
             sx={{ mb: 2 }}
-            icon={<SpaIcon style={{ color: '#499b4a' }} />}
+            icon={<SpaIcon style={{ color: "#499b4a" }} />}
             emptyIcon={<SpaIcon />}
           />
           <TextareaAutosize
-            placeholder='Add a Comment...'
+            placeholder="Add a Comment..."
             value={comment}
-            onChange={e => setComment(e.target.value)}
+            onChange={(e) => setComment(e.target.value)}
             style={{
-              width: '100%',
-              height: '100px',
-              resize: 'none',
+              width: "100%",
+              height: "100px",
+              resize: "none",
               fontFamily: '"Roboto","Helvetica","Arial",sans-serif',
-              fontSize: '1rem',
+              fontSize: "1rem",
               fontWeight: 400,
               lineHeight: 1.5,
-              letterSpacing: '0.00938em',
-              borderRadius: '5px',
-              border: '1px solid #e0e0e0',
-              padding: '10px'
+              letterSpacing: "0.00938em",
+              borderRadius: "5px",
+              border: "1px solid #e0e0e0",
+              padding: "10px",
             }}
           />
           <Button
-            variant='contained'
-            color='primary'
+            variant="contained"
+            color="primary"
             onClick={submitReview}
             sx={{
               mt: 2,
-              bgcolor: '#499b4a',
-              '&:hover': { bgcolor: '#397A3E' }
+              bgcolor: "#499b4a",
+              "&:hover": { bgcolor: "#397A3E" },
             }}
           >
             Submit
@@ -337,12 +345,12 @@ const SerieDetails = () => {
         </Stack>
       </Stack>
 
-      <Typography variant='h5' sx={{ mt: 2 }}>
+      <Typography variant="h5" sx={{ mt: 2 }}>
         Reviews and Comments
       </Typography>
       <ReviewList reviews={reviews} />
     </Container>
-  )
-}
+  );
+};
 
-export default SerieDetails
+export default SerieDetails;
